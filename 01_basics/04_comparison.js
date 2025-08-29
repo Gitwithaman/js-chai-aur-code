2aaa80fd9023aca624435c6ad93444e0ef3bc4f9
Aman khare <147704651+Gitwithaman@users.noreply.github.com>
2025-08-29 14:33:20 +0000

    conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(null>0); // false
+// console.log(null==0); //false
+// console.log(null<=0); 
+//true bcz it treat diff . in comparison null is trated as 0; na hi use kare is tarah ke conversion
+
+// console.log("2"+1);  //21
+// console.log("02"+1);   //021
+//console.log(2+3+"a")  //5a bcz jo samne value datatype hai usi ki hogi
+//console.log("a"+2+3);
+
+
+//console.log("2"===2); //false  also check data type 
+//
