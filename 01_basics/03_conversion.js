27996a5b6f268708e215f10583fe37156b4141a7
Aman khare <147704651+Gitwithaman@users.noreply.github.com>
2025-08-29 13:13:14 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,15 @@
+// let score = 33;
+// console.log(score)
+// console.log(typeof score)
+
+//"33"=>33
+//"33ab"=>NaN
+//true=>1 ; false=>0
+
+let isLoggedin = 1
+let booleanIsLoggedin =Boolean(isLoggedin)
+console.log(booleanIsLoggedin)
+
+//1 => true ; 0=> false
+//""=> false
+//"aman"=> true
