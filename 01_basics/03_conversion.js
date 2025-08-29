13f6a47603e16e12c7b1c5f7a8a913b556277a67
Aman khare <147704651+Gitwithaman@users.noreply.github.com>
2025-08-29 13:38:15 +0000

    sec

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -8,8 +8,24 @@
 
 let isLoggedin = 1
 let booleanIsLoggedin =Boolean(isLoggedin)
-console.log(booleanIsLoggedin)
+//console.log(booleanIsLoggedin)
 
 //1 => true ; 0=> false
 //""=> false
 //"aman"=> true
+
+
+//************  OPERATION ***** */
+
+let value= 3;
+//console.log(-value)
+
+//console.log(3+3) -,/,%,*,**
+
+let str1="aman";
+let str2=" khare";
+//console.log(str1+str2);
+
+let gameCounter= 100;
+++gameCounter;
+console.log(gameCounter)
